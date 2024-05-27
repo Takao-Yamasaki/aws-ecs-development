df12426320632155cbe291831dbcc4f2d8023008
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2024-05-27 12:28:09 +0900

    add:8080

--- environments/security_group.tf
+++ environments/security_group.tf
@@ -10,6 +10,12 @@ resource "aws_security_group" "my-app-frontend-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   // アウトバウンドルール
   egress {
